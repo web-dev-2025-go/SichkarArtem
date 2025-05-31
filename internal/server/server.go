8aadbbbd2c2bd30ca955ddaade8329baaf5b89d5
talafarael <artemk2504@gmail.com>
2025-05-31 18:33:10 +0000

    some change btw

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,9 @@
-package server;
+package server
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+)
 
 type Http struct {
 	server *http.Server
@@ -9,12 +12,19 @@ type Http struct {
 func NewHttp(addr string) *Http {
 	mux := http.NewServeMux()
 
-	handler := NewHandler()
 	server := &http.Server{
 		Addr:    addr,
 		Handler: mux,
 	}
 	return &Http{
-		server: 
+		server: server,
 	}
+}
+
+func (h *Http) Start() error {
+	return h.server.ListenAndServe()
+}
+
+func (h *Http) Stop(ctx context.Context) error {
+	return h.server.Shutdown(ctx)
 }
